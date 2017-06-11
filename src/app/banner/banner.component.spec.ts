46e58e5f6a9b4e57c7c21da359a5813f8ad03e87
Lai <lai32690@gmail.com>
2017-06-11 14:06:46 -0300

    add banner component and test

---
+++ src/app/banner/banner.component.spec.ts
@@ -0,0 +1,42 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
+
+import { BannerComponent } from './banner.component';
+
+describe('BannerComponent', () => {
+    let component: BannerComponent;
+    let fixture: ComponentFixture<BannerComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ BannerComponent ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BannerComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('h1'));
+        el = de.nativeElement;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should display original title', () => {
+        fixture.detectChanges();
+        expect(el.textContent).toContain(component.title);
+    });
+
+    it('should display a different test title', () => {
+        component.title = "Test Title";
+        fixture.detectChanges();
+        expect(el.textContent).toContain("Test Title");
+    });
+});
